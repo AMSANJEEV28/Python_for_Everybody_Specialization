0dd81181f123c94ed3ac00d54fe7533757d46759
A M SANJEEV <amsanjeev28@gmail.com>
2023-06-05 10:47:25 +0530

    Week 3, Python DS

--- 2.Python_Data_Structures/Week3/Assignment7.2.py
+++ 2.Python_Data_Structures/Week3/Assignment7.2.py
@@ -2,9 +2,8 @@
 # X-DSPAM-Confidence:    0.8475
 # Count these lines and extract the floating point values from each of the lines and compute the average of those values and produce an output as shown below. Do not use the sum() function or a variable named sum in your solution.
 # You can download the sample data at http://www.py4e.com/code3/mbox-short.txt when you are testing below enter mbox-short.txt as the file name.
-
-
 # Use the file name mbox-short.txt as the file name
+
 fname = input("Enter file name: ")
 total = 0
 count = 0
