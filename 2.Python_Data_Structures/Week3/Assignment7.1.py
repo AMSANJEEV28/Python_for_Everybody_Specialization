d9208c2e9266716d1fe633fbdbc55f9a54e75ad0
A M SANJEEV <amsanjeev28@gmail.com>
2023-06-05 11:06:44 +0530

    Week 3, Python DS

---
+++ 2.Python_Data_Structures/Week3/Assignment7.1.py
@@ -0,0 +1,8 @@
+# 7.1 Write a program that prompts for a file name, then opens that file and reads through the file, and print the contents of the file in upper case. Use the file words.txt to produce the output below.
+# You can download the sample data at http://www.py4e.com/code3/words.txt
+
+# Use words.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname, 'r')
+for lines in fh:
+    print(lines.strip().upper())
